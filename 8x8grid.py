d1c63ea5d5e4f1b91b5c65db3acaa137fe866043
winkleink <winkleink@gmail.com>
2018-04-06 19:15:47 +0000

    Made 4 x spaces consistent in all files
    Modified files to use 4 spaces for indent

--- 8x8grid.py
+++ 8x8grid.py
@@ -2,17 +2,17 @@ import os, sys
 
 mypath = os.path.dirname(os.path.abspath(__file__))
 if os.path.isfile("/proc/device-tree/hat/product"):
-	file = open("/proc/device-tree/hat/product","r")
-	hat = file.readline()
-	if  hat == "Sense HAT\x00":
-		print('Sense HAT detected')
-		file.close()
-		os.system("/usr/bin/env python3 " + mypath+"/8x8grid-sense.py")
-	elif hat == "Unicorn HAT\x00":
-		print('Unicorn HAT detected')
-		file.close()
-		os.system("/usr/bin/env python3 " + mypath+"/8x8grid-unicorn.py")
+    file = open("/proc/device-tree/hat/product","r")
+    hat = file.readline()
+    if  hat == "Sense HAT\x00":
+        print('Sense HAT detected')
+        file.close()
+        os.system("/usr/bin/env python3 " + mypath+"/8x8grid-sense.py")
+    elif hat == "Unicorn HAT\x00":
+        print('Unicorn HAT detected')
+        file.close()
+        os.system("/usr/bin/env python3 " + mypath+"/8x8grid-unicorn.py")
 else:
-	print("Unknown HAT : " + " Setup for sense_emu")
-	os.system("/usr/bin/env python3 " + mypath+"/8x8grid-no-sense_emu.py")
+    print("Unknown HAT : " + " Setup for sense_emu")
+    os.system("/usr/bin/env python3 " + mypath+"/8x8grid-no-sense_emu.py")
 
